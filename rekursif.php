2729e785b27f016d72c2ad88157fa1838c7269e7
Fitria Riadatul Chusniah <42632527+fitriachusniah@users.noreply.github.com>
2018-09-13 08:36:06 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,46 @@
 <?php
+      /////////////Piramida Bintang/////////////
+        function cetakBaris($bil1)
+        {
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+          if ($bil1 < 1)
+            return;
+          echo "* ";
+          cetakBaris($bil1-1);
+        }
+
+        function cetakPola($bil2)
+        {
+          if ($bil2 < 1)
+            return;
+          cetakBaris($bil2);
+          echo "<br>";
+          cetakPola($bil2-1);
+
+        }
+          cetakPola(5);
+
+echo "<br><br>";
+
+        /////////////Piramida angka/////////////
+        $awal = 1;
+        $batas = 5;
+        function piramid_angka ($i = 1){
+          global $awal;
+          global $batas;
+            echo $awal;
+              $awal++;
+          if ($awal<=$i){
+            piramid_angka($i);
+          }else{
+            echo "<br>";
+            $i++;
+            $awal = 1;
+              if($i<=$batas){
+                  piramid_angka($i);
+              }
+
+          }
+        }
+        piramid_angka();
 ?>
